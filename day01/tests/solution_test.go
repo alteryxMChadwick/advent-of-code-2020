e8bb8751b5a79c435fcd9d6f21ff7c62e5a541db
Michael Chadwick <mchadwick@alteryx.com>
2020-12-01 17:23:26 -0700

    Day01: Beef up tests
    On branch master
    	modified:   solution_test.go

--- day01/tests/solution_test.go
+++ day01/tests/solution_test.go
@@ -4,24 +4,61 @@ import (
 	"testing"
 
 	code "github.com/capsocrates/advent-of-code/day01"
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestDay01(t *testing.T) {
-	exampleNums := []int{
-		1721,
-		979,
-		366,
-		299,
-		675,
-		1456,
-	}
+	t.Run("TestEmpty", func(t *testing.T) {
+		left, right := code.GetPair([]int{})
 
-	left, right := code.GetPair(exampleNums)
+		assert.Equal(t, 0, left)
+		assert.Equal(t, 0, right)
+	})
 
-	require.Equal(t, 1721, left)
-	require.Equal(t, 299, right)
+	t.Run("TestSingleMatchingElement", func(t *testing.T) {
+		left, right := code.GetPair([]int{2020})
 
-	product := code.GetProduct(left, right)
-	require.Equal(t, 514579, product)
+		assert.Equal(t, 0, left)
+		assert.Equal(t, 0, right)
+	})
+
+	t.Run("TestTwoMatchingElements", func(t *testing.T) {
+		left, right := code.GetPair([]int{1010, 1010})
+
+		assert.Equal(t, 1010, left)
+		assert.Equal(t, 1010, right)
+	})
+
+	t.Run("TestTwoMatchingElementsAgain", func(t *testing.T) {
+		left, right := code.GetPair([]int{0, 2020})
+
+		assert.Equal(t, 0, left)
+		assert.Equal(t, 2020, right)
+	})
+
+	t.Run("TestNoMatchingElement", func(t *testing.T) {
+		left, right := code.GetPair([]int{0})
+
+		assert.Equal(t, 0, left)
+		assert.Equal(t, 0, right)
+	})
+
+	t.Run("ExampleSet", func(t *testing.T) {
+		exampleNums := []int{
+			1721,
+			979,
+			366,
+			299,
+			675,
+			1456,
+		}
+
+		left, right := code.GetPair(exampleNums)
+
+		assert.Equal(t, 1721, left)
+		assert.Equal(t, 299, right)
+
+		product := code.GetProduct(left, right)
+		assert.Equal(t, 514579, product)
+	})
 }
